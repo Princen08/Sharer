ac1b5b46fa5145775edc80dbb048df0e760e1e43
Prince Patel <princepatel300282003@gmail.com>
2023-10-24 22:09:40 +0530

    Added cors

--- server/src/main/java/com/sharecode/share/controller/CodeDataController.java
+++ server/src/main/java/com/sharecode/share/controller/CodeDataController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
-@CrossOrigin(value = "http://localhost:3000/")
+@CrossOrigin(value = "https://codesharer.vercel.app/")
 @RestController
 public class CodeDataController {
     @Autowired
