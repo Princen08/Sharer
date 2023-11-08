1dab50120ad6c6852a2534faf0fdddf420edd107
Prince Patel <princepatel300282003@gmail.com>
2023-11-08 09:32:20 +0530

    Last try:

--- server/src/main/java/com/sharecode/share/controller/CodeDataController.java
+++ server/src/main/java/com/sharecode/share/controller/CodeDataController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
-@CrossOrigin
+@CrossOrigin(origins = "https://codesharer.vercel.app")
 @RestController
 public class CodeDataController {
     @Autowired
