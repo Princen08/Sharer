f87f11deed9bf81b2c96c99a95189566ec060e85
Prince Patel <princepatel300282003@gmail.com>
2024-01-05 10:22:48 +0530

    revert back changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -51,29 +51,7 @@ export default function Navbar({ content }) {
     navigator(`/${key.data.key}`);
     window.location.reload();
   }
-  const runCode = async (code, language) => {
-    const options = {
-      method: 'POST',
-      url: 'https://online-code-compiler.p.rapidapi.com/v1/',
-      headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': 'dac444a4e3msh111b5f06d684b6dp1be82bjsn1145dfc51287',
-        'X-RapidAPI-Host': 'online-code-compiler.p.rapidapi.com'
-      },
-      data: {
-        language: language,
-        version: 'latest',
-        code: code,
-        input: null
-      }
-    };
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  
   return (
     <nav
       className="sticky top-0 z-50 bg-black p-4"
@@ -83,12 +61,6 @@ export default function Navbar({ content }) {
         <div className="text-2xl mt-1 font-bold">Sharer</div>
         {content.page == "code" && (
           <div className="flex gap-10">
-            <div className="z-50">
-              <DropdownList defaultValue={currentLanguage} value={currentLanguage} dataKey='id' textField='name' data={languageList} onChange={value => setCurrentLanguage(value)} />
-            </div>
-            <div className="group relative flex justify-end">
-              <button className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded-full" onClick={() => runCode(content?.code, currentLanguage)}>Run</button>
-            </div>
             <div className="group relative flex justify-end">
               <button onClick={() => createNew()}><MaterialSymbolsAddRounded /></button>
               <span className="absolute top-10 scale-0 rounded bg-gray-800 p-4 text-xs text-white group-hover:scale-100">Create New</span>
